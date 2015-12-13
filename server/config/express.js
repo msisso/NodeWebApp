ac7e6c229c2e3b9eaf7a28eeb51c7683d9c0cb1b
Maor <maor@maor-pc.co.so>
2015-12-13 13:59:54 +0200

    socket.io

--- server/config/express.js
+++ server/config/express.js
@@ -9,4 +9,8 @@ module.exports = function(app){
     app.use(express.static('client/assets'));
     app.set('views', path.join(__dirname, '../views'));
     app.set('view engine', 'jade');
+
+    /*app.set('views', path.join(__dirname, '../views'));
+    app.engine('html', require('jade').renderFile);
+    app.set('view engine', 'html');*/
 }
